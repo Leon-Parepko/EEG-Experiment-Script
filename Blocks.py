1768bdfeae752e5b644752cb0e79ec1e0be8677b
Leon-Parepko <l.parepko@innopolis.university>
2023-06-29 20:04:25 +0300

    Add basic architecture.

---
+++ Blocks.py
@@ -0,0 +1,28 @@
+class BlockFabric:
+    """
+    Uses for generation of 4 primitive blocks
+     with different parameters.
+    """
+    pass
+
+class Block:
+    """
+    The abstract class for each of the
+     primitive blocks.
+    """
+    pass
+
+
+
+# 4 primitive blocks
+class PatternBlock(Block):
+    pass
+
+class CommandBlock(Block):
+    pass
+
+class ExecutionBlock(Block):
+    pass
+
+class RestBlock(Block):
+    pass
---
+++ Main.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pygame
+from Images import ImageGenerator as ImGen
+from Blocks import BlockFabric as BlcFab
+
+
+class Main:
+    """
+    The main class.
+    """
+
+    def __init__(self, screen_size, screen_resolution, file_path):
+        self.file_path = file_path
+        self.screen_size = screen_size
+        self.screen_resolution = screen_resolution
+
+        # Generating content
+        self.time_sequence = self.__parse_json()
+        self.block_arr = self.__generate_block_array()
+        self.experiment = self.__generate_experiment()
+
+    def execute_experiment(self):
+        pass
+
+    def __parse_json(self):
+        pass
+
+    def __generate_block_array(self):
+        pass
+
+    def __generate_experiment(self):
+        pass
+
